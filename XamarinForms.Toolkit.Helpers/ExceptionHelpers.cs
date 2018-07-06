c1d76b917f60cff43a9c38a1281d1fa7f803db5d
NUNSYS\gonzalez.zornoza <gonzalez.zornoza@nunsys.com>
2018-07-06 08:29:31 +0200

    -- corregido error en null values

--- XamarinForms.Toolkit.Helpers/ExceptionHelpers.cs
+++ XamarinForms.Toolkit.Helpers/ExceptionHelpers.cs
@@ -25,7 +25,7 @@ namespace XamarinForms.Toolkit.Helpers
             if (_exception == null) return;
 
             // formatear la excepcion en una cadena y registrar en el log
-            LogHelpers.RegisterLog(_exception._getExceptionStringFormat(_infoEx), LogHelpers.EnumLogType.ERROR, typeof(T), _exception.TargetSite.Name);
+            LogHelpers.RegisterLog(_exception._getExceptionStringFormat(_infoEx), LogHelpers.EnumLogType.ERROR, typeof(T), _exception.TargetSite?.Name);
         }
 
         /// <summary>
